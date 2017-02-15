07c4e9b138d1df2d2e29063685ba16827d7b0526
Vishwanath Jayaraman <vishwanathj@hotmail.com>
2017-02-15 00:52:31 -0600

    Demonstration of using for loop

---
+++ loops.py
@@ -0,0 +1,45 @@
+'''
+Objective: Use loops to do simple math.
+
+Task:
+Given an integer, n, print its first 10 multiples. Each multiple nxi(where 1 <= i <= 10) should be printed
+on a new line in the form: n x i = result.
+
+Input Format
+
+A single integer, n.
+
+Constraints
+
+
+Output Format
+
+Print 10 lines of output; each line i (where 1 <= i <= 10) contains the result of nxi in the form:
+n x i = result.
+
+Sample Input
+
+2
+Sample Output
+
+2 x 1 = 2
+2 x 2 = 4
+2 x 3 = 6
+2 x 4 = 8
+2 x 5 = 10
+2 x 6 = 12
+2 x 7 = 14
+2 x 8 = 16
+2 x 9 = 18
+2 x 10 = 20
+'''
+
+#!/bin/python
+
+import sys
+
+
+n = int(raw_input().strip())
+
+for i in range(1, 11):
+    print str(n)+" x "+str(i)+" = "+str(int(n*i))
