036efef1c77cdd92483407f16df4ba3193ebfe1c
Vishwanath Jayaraman <vishwanathj@hotmail.com>
2017-02-14 20:11:38 -0600

    first commit

---
+++ array.py
@@ -0,0 +1,38 @@
+#!/bin/python
+
+import sys
+
+'''
+Objective
+Demonstrates learning about the Array data structure.
+
+Task
+Given an array, A, of  N integers, print A's elements in reverse order as a single line of space-separated numbers.
+
+Input Format
+
+The first line contains an integer, N (the size of our array).
+The second line contains N space-separated integers describing array A's elements.
+
+Constraints
+
+1 <= n <= 1000
+
+1 <= Ai <= 10000, where Ai is the 'i'th integer in the array.
+Output Format
+
+Print the elements of array A in reverse order as a single line of space-separated numbers.
+
+Sample Input
+
+4
+1 4 3 2
+Sample Output
+
+2 3 4 1
+'''
+
+n = int(raw_input().strip())
+arr = map(int,raw_input().strip().split(' '))
+
+print " ".join(str(item) for item in reversed(arr))
