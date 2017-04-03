beb1b09c1b70cc63d39f03873b216d5198da7da4
Vishwanath Jayaraman <vishwanathj@hotmail.com>
2017-04-02 19:48:41 -0500

    Math module exercises

---
+++ python_hackerrank/Math/polar_coordinates.py
@@ -0,0 +1,27 @@
+'''
+Task
+You are given a complex z. Your task is to convert it to polar coordinates.
+
+Input Format
+
+A single line containing the complex number z.
+
+Output Format
+
+Output two lines:
+The first line should contain the value of r.
+The second line should contain the value of pi.
+
+Sample Input
+
+  1+2j
+Sample Output
+
+ 2.23606797749979
+ 1.1071487177940904
+Note: The output should be correct up to 3 decimal places.
+'''
+import cmath
+c = complex(raw_input())
+print float(cmath.polar(c)[0])
+print float(cmath.polar(c)[1])
