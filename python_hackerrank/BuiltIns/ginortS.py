2252d6da9f98cfc059c35f77b5367038864ecd34
Vishwanath Jayaraman <vishwanathj@hotmail.com>
2017-07-09 15:50:55 -0500

    Exercises related to BuiltIns

---
+++ python_hackerrank/BuiltIns/ginortS.py
@@ -0,0 +1,35 @@
+'''
+You are given a string S.
+S contains alphanumeric characters only.
+Your task is to sort the string S in the following manner:
+
+All sorted lowercase letters are ahead of uppercase letters.
+All sorted uppercase letters are ahead of digits.
+All sorted odd digits are ahead of sorted even digits.
+Input Format
+
+A single line of input contains the string S.
+
+Constraints
+
+0 < len(S) < 1000
+
+Output Format
+
+Output the sorted string S.
+
+Sample Input
+
+Sorting1234
+Sample Output
+
+ginortS1324
+Note:
+a) Using join, for or while anywhere in your code, even as substrings, will result in a score of 0.
+b) You can only use one sorted function in your code. A 0 score will be awarded for using sorted more than once.
+
+Hint: Success is not the key, but key is success.
+'''
+from __future__ import print_function
+import string
+print(*sorted(raw_input(), key=(string.ascii_letters + '1357902468').index), sep='')
