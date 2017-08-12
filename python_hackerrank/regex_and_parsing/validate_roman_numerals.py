d1578a97ef0a0b4263cfd85800ac79543e7d7aaa
Vishwanath Jayaraman <vishwanathj@hotmail.com>
2017-08-12 10:57:18 -0500

    regex related exercises

---
+++ python_hackerrank/regex_and_parsing/validate_roman_numerals.py
@@ -0,0 +1,30 @@
+'''
+You are given a string, and you have to validate whether it's a valid Roman numeral. If it is valid, print True. Otherwise, print False. Try to create a regular expression for a valid Roman numeral.
+
+Input Format
+
+A single line of input containing a string of Roman characters.
+
+Output Format
+
+Output a single line containing True or False according to the instructions above.
+
+Constraints
+
+The number will be between 1 and 3999 (both included).
+
+Sample Input
+
+CDXXI
+Sample Output
+
+True
+'''
+
+import re
+
+thousand = 'M{0,3}'
+hundred = '(C[MD]|D?C{0,3})'
+ten = '(X[CL]|L?X{0,3})'
+digit = '(I[VX]|V?I{0,3})'
+print (bool(re.match(thousand + hundred+ten+digit +'$', raw_input())))
